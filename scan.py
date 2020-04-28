267575e47ce69819d6915c8f93669aff2e67aad4
0rientd <0rientd>
2020-04-27 21:07:49 -0300

    Made the projetct almost finished. Everthing works!

--- scan.py
+++ scan.py
@@ -1,27 +1,26 @@
 from class_get_serial import GetSerialUSB
+from sql import sql_db as sql
 from time import sleep
 import ctypes
 
 def scan_to_lock():
-    _lock = False
     lista_de_serial = GetSerialUSB().init()
-    if "2011020600004351" not in lista_de_serial:
-        lock = True
-        print(lock)
-        ctypes.windll.user32.LockWorkStation()
+    all_table = sql().get_table()
+    all_table.pop()
+
+    for usb in all_table:
+        if usb[3] == 1:
+            if usb[2] not in lista_de_serial:
+                ctypes.windll.user32.LockWorkStation()
+            elif usb[2] in lista_de_serial:
+                print(f"Usb chave conectado | Chave => {usb[2]}")
+                pass
+            else:
+                print("Alguma coisa errada aconteceu 1")
+        else:
+            print(f"Serial {usb[2]} encontrado mas não ativo")
 
     sleep(1)
     scan_to_lock()
-
-def scan_to_unlock():
-    _lock = True
-    lista_de_serial = GetSerialUSB().init()
-    if "2011020600004351" in lista_de_serial:
-        lock = False
-        print(lock)
-        scan_to_lock()
-
-    sleep(1)
-    scan_to_unlock()
 
 scan_to_lock()
