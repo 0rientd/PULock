5e6606541c80dfd5ebe04ece74a0db198d222e89
0rientd <0rientd>
2020-04-28 20:17:23 -0300

    Maybe finished the project! Still need to make the install..

--- class_tkinter.py
+++ class_tkinter.py
@@ -12,15 +12,28 @@ class Application(tk.Frame):
             usbs = list(usbs)
             self.create_widgets(usbs)
 
-        self.quit = tk.Button(self, text="QUIT", fg="red", command=self.master.destroy)
-        self.quit.pack(side="bottom")
+        self.texto = tk.Label(self, text="GREEN is activated and RED is deactivated",
+                              font=("Calibri", "8", "bold"), bg="#61656E", fg="purple")
+        self.texto.pack(side="bottom")
+
+        self.quit = tk.Button(self, text="SAIR", fg="cyan", command=self.master.quit)
+        self.quit["font"] = ("Calibri", "11", "bold")
+        self.quit["bg"] = "grey"
+        self.quit.pack(side="bottom", pady=50)
 
     def create_widgets(self, usbs):
         btn = tk.Button(self)
+        if usbs[3] == 1:
+            btn["fg"] = "green"
+        else:
+            btn["fg"] = "red"
         btn["text"] = usbs[1]
         btn["command"] = lambda: [btn.destroy(), self.create_widgets(self.acao_botao(usbs))]
+        btn["font"] = ("Calibri", "11", "bold")
+        btn["bd"] = 5
+        btn["padx"] = 65
 
-        btn.pack()
+        btn.pack(pady=3)
 
     def acao_botao(self, usb):
         print(f"Atualizando status do modelo => {usb[1]} com serial {usb[2]}")
@@ -29,7 +42,10 @@ class Application(tk.Frame):
         return usb
 
 root = tk.Tk()
+root.configure(background = '#61656E')
 app = Application(master=root)
+app.configure(background = '#61656E')
 app.master.title("PULock")
 app.master.minsize(400, 250)
+
 app.mainloop()
